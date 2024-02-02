bebc9fa956f82158b248bc86aee349c89660d495
Jingbin Wang <jingtechy@gmail.com>
2024-02-02 14:05:33 +1300

    Update product page test

--- src/main/java/saucedemo/pages/ProductPage.java
+++ src/main/java/saucedemo/pages/ProductPage.java
@@ -2,13 +2,20 @@ package saucedemo.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ProductPage {
     private final WebDriver driver;
+    private WebElement productButton;
     public ProductPage(WebDriver driver)  { this.driver = driver; }
 
     public void clickProductTitle(String product) {
         driver.findElement(By.xpath("//div[@class='inventory_item_name ' and text()='" + product + "']")).click();
+    }
+
+    public boolean isButtonDisplayed(String button) {
+        productButton = driver.findElement(By.xpath("//button[text()='" + button + "']"));
+        return productButton.isDisplayed();
     }
 
 }
